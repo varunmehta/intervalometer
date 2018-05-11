74274db4192609f6e681b2ad42c7e1b7db6f884d
Varun Mehta <vmehta@sintecmedia.com>
2018-05-11 17:35:25 -0400

    Added prints to python

--- intervalometer.py
+++ intervalometer.py
@@ -15,17 +15,25 @@ camera = PiCamera()
 FILE_PATH='/data'
 
 # create dir first time
-if not os.path.exists(FILE_PATH):
-    os.makedirs(FILE_PATH)
+# if not os.path.exists(FILE_PATH):
+#    os.makedirs(FILE_PATH)
+
+print ("Program started")
 
 # initalize camera
 camera.start_preview()
 sleep(10)
 
+print ("sleeping... ")
+
 # infinite loop to click pictures
 while True:
     file_name = int(round(time.time()))
+    print ("filename" + file_name)
     camera.capture(FILE_PATH + '/' + file_name + '.jpg')
     # click a picture every 5 seconds.
+
+    print ("Captured, now sleeping...")
     sleep(5)
 camera.stop_preview()
+print ("Program exited")
